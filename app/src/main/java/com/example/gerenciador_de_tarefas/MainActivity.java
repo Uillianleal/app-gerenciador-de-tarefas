d7b5415c6ab95e5ba4cae27149a519470d965e5c
Uillian Leal <willianleal52@hotmail.com>
2024-04-10 11:37:09 -0300

    Layout nova tarefa

--- app/src/main/java/com/example/gerenciador_de_tarefas/MainActivity.java
+++ app/src/main/java/com/example/gerenciador_de_tarefas/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.gerenciador_de_tarefas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Verifique se a aba selecionada é a que você deseja (por exemplo, a aba "Nova Tarefa")
+                if (tab.getText().equals("Nova Tarefa")) {
+                    Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Aqui você pode implementar o comportamento quando uma aba é desselecionada, se necessário
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Aqui você pode implementar o comportamento quando uma aba já selecionada é clicada novamente, se necessário
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/gerenciador_de_tarefas/NewTaskActivity.java
@@ -0,0 +1,61 @@
+package com.example.gerenciador_de_tarefas;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_task);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final EditText editTextTaskDate = findViewById(R.id.editTextTaskDate);
+        editTextTaskDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Mudar o locale para português
+                Locale locale = new Locale("pt", "BR");
+                Locale.setDefault(locale);
+                calendar.setFirstDayOfWeek(Calendar.SUNDAY);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        NewTaskActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                                editTextTaskDate.setText(selectedDate);
+                            }
+                        }, year, month, day);
+
+                // Defina o locale do datePickerDialog
+                datePickerDialog.getDatePicker().setFirstDayOfWeek(Calendar.SUNDAY);
+                datePickerDialog.show();
+            }
+        });
+    }
+}
